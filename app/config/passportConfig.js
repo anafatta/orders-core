106c3ad18a577ec6145e12ea47359d1bef1bd374
Analia Fatta <afatta@lendingclub.com>
2018-11-07 09:03:35 -0300

    new messages and signup without pass

--- app/config/passportConfig.js
+++ app/config/passportConfig.js
@@ -3,11 +3,11 @@ const passportJWT = require("passport-jwt");
 const JWTStrategy   = passportJWT.Strategy;
 const extractJWT = passportJWT.ExtractJwt;
 const db = require('../models');
-const SALT_WORK_FACTOR = 12;
+const config = require('./config.json');
 
 passport.use(new JWTStrategy({
         jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey   : 'your_jwt_secret' // Put this value in a environmet var
+        secretOrKey   : config.jwt_secret // Put this value in a environmet var
     },
     function (jwtPayload, cb) {
         return UserModel.findOneById(jwtPayload.id)
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,7 +1,8 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 var bcrypt = require('bcrypt-nodejs');
-var SALT_WORK_FACTOR = require('../config/passportConfig').SALT_WORK_FACTOR;
+var message = require('../config/messages.json');
+const config = require('../config/config.json');
 
 
 module.exports = {
@@ -14,19 +15,25 @@ module.exports = {
             att['where'] = { email: req.body.username }
             db.ssecur_user.findOne(att).then(user => {
                 if (user) {
-                    return res.status(200).send({ message: 'The user already exists' })
+                    return res.status(200).send({auth: false, message: message.auth.user_already_exists })
                 }
-            }).catch(error => res.status(500).send('Error 2' + error));
-            var salt = bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+            }).catch(error => {
+                console.log('error in user findOne: ' + error);
+                res.status(500).send({auth: false, message : message.auth.unexpected_error})
+            }
+            );
+            // FOR NOW THE PASSWORD ALWAYS SHOULD BE BLANK
+             /* var salt = bcrypt.genSalt(config.salt_work_factor, function (err, salt) {
                 return salt;
             });
             bcrypt.hash(req.body.username, salt, null, function (err, hash) {
                 if (err) {
-                    return res.status(500).send({ message : "Unexpected Error" })
-                }
+                    console.log('error in bcrypt hash: ' + error);
+                    return res.status(500).send({ auth: false, message : message.auth.unexpected_error })
+                } */
                 var new_user = {
                     email: req.body.username,
-                    pwdhash: hash,
+                    pwdhash: '',
                     password:req.body.lastname, // NOT BE NULL
                     lastname: req.body.lastname,
                     firstname: req.body.firstname,
@@ -35,44 +42,56 @@ module.exports = {
                     // OTHERS FIELDS?
                 };
                 db.ssecur_user.create(new_user).then(userCreated => {
-                    var token = jwt.sign({ id: userCreated._id }, 'your_jwt_secret', {
+                 /*    var token = jwt.sign({ id: userCreated._id }, config.jwt_secret, {
                         expiresIn: 86400 // expires in 24 hours
+                    }); */
+                    return res.status(200).send({ auth: false, message: message.user_created })
+                }).catch(error => 
+                    {
+                        console.log('error in user create: ' + error)
+                        res.status(500).send({auth: false, message:  message.auth.unexpected_error})
                     });
-                    return res.status(200).send({ auth: true, token: token })
-                }).catch(error => res.status(500).send('Error 1' + error));
-            });
+           // });
 
         }
     },
 
     login(req, res) {
         att = {};
-        if (req.body.username) {
-            delete att.attributes
-            att['attributes'] = ['nro', 'email', 'firstname', 'lastname', 'pwdhash'];
-            if (!att['where']) { att['where'] = {} }
-            att['where'] = { email: req.body.username }
-            db.ssecur_user.findOne(att)
-                .then(user => {
-                    //VALIDATE PASSWORD
-                    if (!user) {
-                        return res.status(400).send({ auth: false, message: "Invalid user" })
-                    }
-                    bcrypt.compare(req.body.password, user.pwdhash, function (err, isValid) {
-                        if (err) {
-                            return res.status(500).send({ auth: false, message: "Unexpected Error" })
-                        }
-                        if (isValid) {
-                            var token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
-                                expiresIn: 86400 // expires in 24 hours
-                            });
-                            return res.status(200).send({ auth: true, token: token });
-                        } else {
-                            return res.status(400).send({ auth: false, message: "Invalid password" })
-                        }
-                    });
-                }).catch(error => res.status(500).send(error));
+        if (!req.body.username) {
+            return res.status(400).send({ auth: false, message: message.auth.invalid_user })
         }
+        if(!req.body.password){
+            return res.status(400).send({ auth: false, message: message.auth.invalid_pass })
+        }
+        delete att.attributes
+        att['attributes'] = ['nro', 'email', 'firstname', 'lastname', 'pwdhash'];
+        if (!att['where']) { att['where'] = {} }
+        att['where'] = { email: req.body.username }
+        db.ssecur_user.findOne(att)
+            .then(user => {
+                //VALIDATE PASSWORD
+                if (!user) {
+                    return res.status(400).send({ auth: false, message: message.auth.invalid_user })
+                }
+                bcrypt.compare(req.body.password, user.pwdhash, function (err, isValid) {
+                    if (err) {
+                        console.log('error in bcrypt compare: ' + error)
+                        return res.status(500).send({ auth: false, message: message.auth.unexpected_error })
+                    }
+                    if (isValid) {
+                        var token = jwt.sign({ id: user._id }, config.jwt_secret, {
+                            expiresIn: 86400 // expires in 24 hours
+                        });
+                        return res.status(200).send({ auth: true, token: token });
+                    } else {
+                        return res.status(400).send({ auth: false, message: message.auth.invalid_pass })
+                    }
+                });
+            }).catch(error => {
+                console.log('error in user findOne: ' + error)
+                res.status(500).send({ auth: false, message: message.auth.unexpected_error })
+            });
     },
 
     changePassword(req, res) {
@@ -81,7 +100,7 @@ module.exports = {
         var newPassword = req.body.newpassword;
        console.log('    req.body.username:' + req.body.username + '    req.body.oldpassword:' + req.body.oldpassword + '    req.body.newpassword:' + req.body.newpassword)
         if (!username || !newPassword) {
-            return res.status(412).send({ message: "Invalid user or new password" })
+            return res.status(412).send({auth:false,  message: message.auth.invalid_pass + ' or ' + message.auth.invalid_user})
         }
         att = {};
         att['attributes'] = ['nro', 'email', 'firstname', 'lastname', 'pwdhash'];
@@ -92,42 +111,43 @@ module.exports = {
                 var isValid;
                 console.log('  user.pwdhash: ' + user.pwdhash)
                 if (user.pwdhash) {
-                    isValid = bcrypt.compare(oldPassword, user.pwdhash, function (err, isValid) {
-                        if (err) {
-                            return res.status(500).send({ auth: false, message: "Unexpected Error" })
-                        }
-                        console.log('is valid1:' + isValid)
-                        return isValid;
-                    });
+                    isValid = bcrypt.compareSync(oldPassword, user.pwdhash);
                 } else {
                     //Pass Blank -- reset case
                     isValid = true;
                 }
                   console.log('is valid1:' + isValid)
                 if (isValid) {
-                    var salt = bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+                    var salt = bcrypt.genSalt(config.salt_work_factor, function (err, salt) {
                         return salt;
                     });
                     bcrypt.hash(newPassword, salt, null, function (err, hash) {
                         if (err) {
-                            return res.status(500).send({ message: "Unexpected Error" })
+                            console.log('error in bcrypt hash: ' + error)
+                            return res.status(500).send({auth : false, message: message.auth.unexpected_error })
                         }
                         db.ssecur_user.update({ pwdhash: hash }, { where: { email: username } })
                             .then(user => {
-                                var token = jwt.sign({ id: user._id }, 'your_jwt_secret', {
+                                var token = jwt.sign({ id: user._id }, config.jwt_secret, {
                                     expiresIn: 86400 // expires in 24 hours
                                 });
                                 return res.status(200).send({ auth: true, token: token });
                             })
-                            .catch(error => res.status(500).send('Error updatinf user' + error));
+                            .catch(error => {
+                                console.log('error in user update: ' + error)
+                                res.status(500).send({ auth: false , message :  message.auth.unexpected_error})
+                            });
                     });
                 } else {
-                    return res.status(400).send({ auth: false, message: "Invalid password" })
+                    return res.status(400).send({ auth: false, message: message.auth.invalid_pass })
                 }
             } else {
-                return res.status(400).send({ auth: false, message: "Invalid user" })
+                return res.status(400).send({ auth: false, message: message.auth.invalid_user })
             }
-        }).catch(error => res.status(500).send('Error trying to find user ' + error));
+        }).catch(error => {
+            console.log('error in user findOne: ' + error)
+            res.status(500).send({ auth: false , message :  message.auth.unexpected_error})
+        });
     }
 
 
